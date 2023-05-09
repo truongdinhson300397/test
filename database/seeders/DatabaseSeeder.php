e66dc88bcb01ceec3b7b525514f007c782191351
Truong Son <truong.son@vand.vn>
2023-05-09 11:26:32 +0700

    remove migrate

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder {
      */
     public function run() {
         $this->call(RootAdminSeeder::class);
-        $this->call(MenuTableSeeder::class);
-        $this->call(AdminClassMenuTableSeeder::class);
+//        $this->call(MenuTableSeeder::class);
+//        $this->call(AdminClassMenuTableSeeder::class);
         $this->call(UserTableSeeder::class);
     }
 }
